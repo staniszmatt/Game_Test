f6601fd9db9d3f1860b188a0d137d6fa486f087d
staniszmatt <matthewpstaniszewski@gmail.com>
2018-01-23 20:38:11 -0800

    Initial setup, partial done

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict"; 
+
+const express = require('express');
+const app = express();
+const serv = require('http').Server(app);
+
+app.get('/', function(reg, res){res.sendFile(__dirname + '/client/index.html');});
+app.use('/client', express.static(__dirname + '/client'));
+
+serv.listen(2000);
