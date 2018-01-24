76ba3c8155e113e46af1b1a1e88218df22ab1a61
staniszmatt <matthewpstaniszewski@gmail.com>
2018-01-24 12:48:20 -0800

    Adding files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-"use strict"; 
+"use strict";           //Help keep the formatting in check 
 
-const express = require('express');
-const app = express();
+const express = require('express');     //TO use express for file communication 
+const app = express();                  // 
 const serv = require('http').Server(app);
 
 app.get('/', function(reg, res){res.sendFile(__dirname + '/client/index.html');});
